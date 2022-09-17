d29d7b3470183365b3d12fdd5f403c2fcd0d692b
wariz <wariz1359@gmail.com>
2022-09-17 18:35:19 +0200

    upgrade style

--- jeu.py
+++ jeu.py
@@ -1,39 +1,51 @@
-from colorama import Fore, Back, Style
+from colorama import Fore, Back, Style, deinit, init, Cursor
 from msvcrt import getch
 from random import*
 
-nb_cases_x = 6
-nb_cases_y = 6
+nb_cases_x = 10
+nb_cases_y = 10
 
 player_x = 6
 player_y = 3
 
 
-mob_x = [0, 0, 6, 6]
-mob_y = [0, 6, 0, 6]
+mob_x = [0, 0]
+mob_y = [0, nb_cases_x - 1]
 
+#mob_x = [0, 0, nb_cases_x - 1, nb_cases_x - 1]
+#mob_y = [0, nb_cases_y - 1, 0, nb_cases_y - 1]
+
+compteur_tour = 0
+compteur_points = 0
+
+piece_x = randint(0, nb_cases_x)
+piece_y = randint(0, nb_cases_y)
 
 def actualisation():
-    print("--------------------------")
-    for x in range(7):
-        for y in range(7):
+    print(Cursor.POS(0, 0))
+    print("—————————————————————————————")
+    for x in range(nb_cases_x):
+        for y in range(nb_cases_y):
             mob_nb = 0
             for i in range(len(mob_x)):
                 if x == mob_x[i] and y == mob_y[i]:
                     mob_nb += 1
             if mob_nb >= 1:
                 if mob_nb == 1:
-                    print(Fore.YELLOW + " " + str(mob_nb) + Style.RESET_ALL, end=" ")
+                    print(Fore.YELLOW + " " + str(mob_nb) + " " + Style.RESET_ALL, end="")
                 elif mob_nb == 2:
-                    print(Fore.MAGENTA + " " + str(mob_nb) + Style.RESET_ALL, end=" ")
+                    print(Fore.MAGENTA + " " + str(mob_nb) + " " + Style.RESET_ALL, end="")
                 else:
-                    print(Fore.RED + " " + str(mob_nb) + Style.RESET_ALL, end=" ")
+                    print(Fore.RED + " " + str(mob_nb) + " " + Style.RESET_ALL, end="")
             elif x == player_x and y == player_y:
-                print(Fore.BLUE + " O" + Style.RESET_ALL, end=" ")
+                print(Fore.BLUE + " ● " + Style.RESET_ALL, end="")
+            elif compteur_tour >= 2 and x == piece_x and y == piece_y:
+                print(Fore.GREEN + " * " + Style.RESET_ALL, end="")
             else:
-                print(" .", end=" ")
+                print(" . " + Style.RESET_ALL, end="")
         print("")
-
+    print(Cursor.POS(35, 8) + Style.BRIGHT + "Infos :" + Style.RESET_ALL, "Tour :", compteur_tour + 1, "– Monstres :", len(mob_x), '– Points :', compteur_points)
+    print(Cursor.POS(0, nb_cases_y + 2))
 
 def deplacement_mob():
     global mob_x, mob_y
@@ -81,41 +93,46 @@ def deplacement_player():
     global player_x, player_y
     # print("Saisissez g, d, b, h")
     move = getch().lower()
-    # print(move)
     if move == b'q':
         if player_y != 0:
             player_y -= 1
         else:
-            print(Fore.RED + "Vous ne pouvez pas traverser le mur, réessayez" + Style.RESET_ALL)
+            print(Fore.RED + "Vous ne pouvez pas traverser le mur" + Style.RESET_ALL)
     elif move == b'd':
-        if player_y != nb_cases_y:
+        if player_y != nb_cases_y-1:
             player_y += 1
         else:
-            print(Fore.RED + "Vous ne pouvez pas traverser le mur, réessayez" + Style.RESET_ALL)
+            print(Fore.RED + "Vous ne pouvez pas traverser le mur" + Style.RESET_ALL)
     elif move == b'z':
         if player_x != 0:
             player_x -= 1
         else:
-            print(Fore.RED + "Vous ne pouvez pas traverser le mur, réessayez" + Style.RESET_ALL)
+            print(Fore.RED + "Vous ne pouvez pas traverser le mur" + Style.RESET_ALL)
     elif move == b's':
-        if player_x != nb_cases_x:
+        if player_x != nb_cases_x-1:
             player_x += 1
         else:
-            print(Fore.RED + "Vous ne pouvez pas traverser le mur, réessayez" + Style.RESET_ALL)
+            print(Fore.RED + "Vous ne pouvez pas traverser le mur" + Style.RESET_ALL)
     else:
-        print("saisie invalide")
+        print("Déplacement invalide, il faut taper z,q,s,d")
 
 def test_collision():
+    global compteur_points, piece_x, piece_y
     # on compare les postitions des mobs avec celle du joueur
     for i in range(len(mob_x)):
         if player_y == mob_y[i] and player_x == mob_x[i]:
-            exit('Vous avez perdu')
+            exit('╠═══╍╍ Vous avez perdu ╍╍═══╣')
+        elif player_x == piece_x and player_y == piece_y:
+            compteur_points += 1
+            piece_x = randint(0, nb_cases_x)
+            piece_y = randint(0, nb_cases_y)
 
 def main():
-    global player_x, player_y
-    print("LE JEU COMMENCE")
+    global player_x, player_y, compteur_tour
+    print('\033[3C' + '╱╳╲ LE JEU COMMENCE ╱╳╲')
     actualisation()
     while True:
+        compteur_tour += 1
         deplacement_player()
         test_collision()
         deplacement_mob()
